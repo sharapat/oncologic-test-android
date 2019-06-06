012ce8d31ce78312bf8383d239b94e4cc7d2249d
sharapat <kalabaevshj@gmail.com>
2019-06-06 18:32:36 +0500

    begin

--- app/src/main/java/uz/tuit/oncologic/di/Injections.kt
+++ app/src/main/java/uz/tuit/oncologic/di/Injections.kt
@@ -1,31 +1,19 @@
 package uz.tuit.oncologic.di
 
 import com.google.firebase.firestore.FirebaseFirestore
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import uz.tuit.oncologic.data.AppRepository
-import uz.tuit.oncologic.data.network.AddCookiesInterceptor
-import uz.tuit.oncologic.data.network.ApiService
-import uz.tuit.oncologic.data.network.ReceivedCookiesInterceptor
 import uz.tuit.oncologic.helper.JsoupHelper
 import uz.tuit.oncologic.helper.SharedPreferencesHelper
 import uz.tuit.oncologic.ui.auth.AuthViewModel
 import uz.tuit.oncologic.ui.main.MainViewModel
 import uz.tuit.oncologic.ui.result.ResultViewModel
-import java.util.concurrent.TimeUnit
 
 private const val baseUrl = /*"https://test.requestcatcher.com"*/ "http://xn----8sb4anfhdi.xn--p1ai"
 
 val networkModule = module {
-    single { provideOkHttpClient(get(), get()) }
-    single { providesRetrofit(get()) }
-    single { providesApiService(get()) }
     single { FirebaseFirestore.getInstance() }
 }
 
@@ -42,41 +30,6 @@ val viewModelModule = module {
 
 val preferencesModule = module {
     single { SharedPreferencesHelper(androidContext()) }
-}
-
-val interceptorModule = module {
-    single { ReceivedCookiesInterceptor(get()) }
-    single { AddCookiesInterceptor(get()) }
-}
-
-fun provideOkHttpClient(
-    receivedCookiesInterceptor: ReceivedCookiesInterceptor,
-    addCookiesInterceptor: AddCookiesInterceptor) : OkHttpClient {
-
-    val interceptor = HttpLoggingInterceptor()
-    interceptor.level = HttpLoggingInterceptor.Level.BODY
-
-    return OkHttpClient.Builder()
-        .connectTimeout(60, TimeUnit.SECONDS)
-        .readTimeout(60, TimeUnit.SECONDS)
-        .writeTimeout(60, TimeUnit.SECONDS)
-        .retryOnConnectionFailure(false)
-        .addInterceptor(interceptor)
-        .addInterceptor(receivedCookiesInterceptor)
-        .addInterceptor(addCookiesInterceptor)
-        .build()
-}
-
-fun providesRetrofit(okHttpClient: OkHttpClient): Retrofit =
-    Retrofit.Builder()
-        .addConverterFactory(ScalarsConverterFactory.create())
-        .baseUrl(baseUrl)
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .client(okHttpClient)
-        .build()
-
-fun providesApiService(retrofit: Retrofit): ApiService {
-    return retrofit.create(ApiService::class.java)
 }
 
 
